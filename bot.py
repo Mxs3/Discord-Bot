3af47b8e1786d609d23495bdb12154a22c94a861
Masen <77242074+Mxsen-sys@users.noreply.github.com>
2021-01-14 22:49:19 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ from discord.utils import get
 from discord.ext.commands import MissingPermissions
 from colorama import Fore, Style
 
-TOKEN = 'Nzk4NDY0OTE4MTM5NTY4MTM5.X_1aZw.gUzLzgf6cbgS91lrh6NSTHHBN8E'
+TOKEN = #Bot token goes here
 cmd_prefix = '!'
 client = commands.Bot(command_prefix=cmd_prefix)
 general_chat_id = 787473942348693547
